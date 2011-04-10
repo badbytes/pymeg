7a59347f0e084074d7d20c70d6b74feee5bf5eac
DanC <danc@badbytes.net>
2011-04-09 23:39:01 -0600

    spinner play

---
+++ gui/gtk/spinner.py
@@ -0,0 +1,62 @@
+#
+#       Copyright 2010 dan collins <danc@badbytes.net>
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+import sys,time
+
+try:
+    import pygtk
+    pygtk.require("2.0")
+except:
+    pass
+try:
+    import gtk
+    import gtk.glade
+except:
+    print("GTK Not Availible")
+    sys.exit(1)
+
+class template:
+    def __init__(self):
+        self.builder = gtk.Builder()
+        self.builder.add_from_file("spinner.glade")
+        self.window = self.builder.get_object("window1")
+        self.spin = self.builder.get_object("spinner1")
+
+        dic = {
+            "on_button1_clicked" : self.test,
+            }
+
+        self.builder.connect_signals(dic)
+        
+
+    def test(self,widget):
+        print 'test'
+        self.builder.get_object("spinner1").stop()
+
+    def datahandler(data=None):
+        pass
+
+
+if __name__ == "__main__":
+    mainwindow = template()
+    mainwindow.window.show()
+    spin = mainwindow.builder.get_object("spinner1")
+    spin.start()
+    #time.sleep(5)
+    #spin.stop()
+    print 'testing'
+    gtk.main()
