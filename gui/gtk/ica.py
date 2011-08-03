18b8181e9e9d6e0a967a8bd58ca4df9926424c49
DanC <danc@badbytes.net>
2011-08-03 16:36:23 -0600

    ICA

--- gui/gtk/ica.py
+++ gui/gtk/ica.py
@@ -16,7 +16,8 @@
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
-import sys,os
+import sys,os,mdp
+from numpy import *
 
 try:
     import pygtk
@@ -30,28 +31,80 @@ except:
     print("GTK Not Availible")
     sys.exit(1)
 
+'''approach='defl', g='pow3', guess=None, fine_g='pow3', mu=1,
+stabilization=False, sample_size=1, fine_tanh=1, fine_gaus=1, max_it=1000,
+max_it_fine=100, failures=5, limit=0.001, verbose=False, whitened=False, white_comp=None,
+white_parm=None, input_dim=None, dtype=None)'''
+
 class setup:
-    def __init__(self):
+    def __init__(self,data=None):
         self.builder = gtk.Builder()
         self.builder.add_from_file(os.path.splitext(__file__)[0]+".glade")
         self.window = self.builder.get_object("window")
-        self.LS = gtk.ListStore(str)
 
         dic = {
-            "on_button1_clicked" : self.test,
+            "on_button_apply_clicked" : self.get_options,
             }
 
         self.builder.connect_signals(dic)
+        self.data = data
 
-    def test(self,widget):
-        print('test')
 
-    def datahandler(data=None):
-        pass
+    def get_options(self,widget):
+        opts = {}
+        print 'hello'
+        names = self.builder.get_object("box_names").get_children()
+        vals = self.builder.get_object("box_values").get_children()
+
+
+        for i in arange(len(vals)):
+            if vals[i].get_name() == 'GtkComboBox':
+                res = vals[i].get_active_text()
+            if vals[i].get_name() == 'GtkEntry':
+                try: res = float32(vals[i].get_text())
+                except ValueError: res = vals[i].get_text()
+
+            opts[names[i].get_text()] = res
+
+        print opts
+
+
+        #Run ICA
+        ica = mdp.nodes.FastICANode(white_comp=opts['white_comp'],approach=opts['approach'], g=opts['g'], fine_g=opts['fine_g'],\
+        mu=opts['mu'], stabilization=opts['stabilization'], sample_size=opts['sample_size'], fine_tanh=opts['fine_tanh'], fine_gaus=opts['fine_gaus'], \
+        max_it=int(opts['max_it']), max_it_fine=int(opts['max_it_fine']), failures=opts['failures'], limit=opts['limit'],verbose=True)
+        #, whitened=opts['whitened'])#, \
+        #white_parm=opts['white_parm'])
+
+        '''ica = mdp.nodes.FastICANode(white_comp=10,approach='defl', g='pow3', guess=None, fine_g='pow3', mu=1, stabilization=False, sample_size=1, fine_tanh=1, fine_gaus=1, max_it=10000, max_it_fine=100, failures=5, limit=0.001, verbose=True)'''
+
+
+        ica.train(self.data)
+        comp = ica.execute(self.data)
+        #z = dot(self.data,ica.get_recmatrix().T)
+        #z = z-z[0]
+        print 'done'
+        from pylab import *
+        subplot(2,1,1)
+        plot(comp);
+        subplot(2,1,2)
+        plot(self.data-self.data[0]);
+        show()
+
+
+
+
 
 
 if __name__ == "__main__":
-    mainwindow = setup()
+    from pdf2py import pdf
+    p = pdf.read('/home/danc/data/meg/0611piez/e,rfhp1.0Hz,ra,f50lp,o')
+    p.data.setchannels('meg')
+    p.data.getdata(0,p.data.pnts_in_file)
+
+
+    mainwindow = setup(data=p.data.data_block)
     mainwindow.window.show()
+
     print('testing')
     gtk.main()
