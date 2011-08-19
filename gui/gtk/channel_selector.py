afb39ecba9f4271a37738e496caa29e20c4c8fd3
DanC <danc@badbytes.net>
2011-08-18 21:32:50 -0600

    work on gui channel selector

--- gui/gtk/channel_selector.py
+++ gui/gtk/channel_selector.py
@@ -39,10 +39,12 @@ from matplotlib.figure import Figure
 from matplotlib.lines import Line2D
 
 class setup_gui:
-    def __init__(self):
+    def __init__(self,chanlocs=None):
         self.builder = gtk.Builder()
         self.builder.add_from_file(os.path.splitext(__file__)[0]+".glade")
         self.window = self.builder.get_object("window")
+        try: self.data = chanlocs
+        except: pass
 
         dic = {
             #"on_auto_select_toggled" : self.test,
@@ -50,7 +52,7 @@ class setup_gui:
             "showpopupmenu" : self.showpopupmenu,
             "on_select_toggled" : self.select_toggled,
             #"on_checked_toggled" : self.select_checked,
-
+            "on_view_label_toggle" : self.view_label_toggled,
             }
 
         self.builder.connect_signals(dic)
@@ -83,7 +85,7 @@ class setup_gui:
         self.dataList = gtk.ListStore(int,str,gobject.TYPE_BOOLEAN)
         self.AddListColumn('Number', 0, self.View)
         self.AddListColumn('Label', 1, self.View)
-        self.AddBoolColumn('test', 2, self.View)
+        self.AddBoolColumn('Select', 2, self.View)
 
 
         self.numchannels=np.size(self.data,1)#300
@@ -150,13 +152,13 @@ class setup_gui:
 
         try: pass#self.axes.clear()
         except: pass
-        self.axes.axis('off')
+        #self.axes.axis('off')
         #self.axes.scatter(self.data[1],self.data[0],marker='o',facecolors='none');
         liststore,iter = self.View.get_selection().get_selected_rows()
         chanind = []
         for i in iter:
             x = liststore[i][0]
-            chanind.append(int(liststore[i][0]))
+            #chanind.append(int(liststore[i][0]))
             chanind.append(int(liststore[i][0]))
             if liststore[i][2] == False:
                 scat = self.axes.scatter(self.data[1,x],self.data[0,x],marker='o',color='magenta')
@@ -181,7 +183,18 @@ class setup_gui:
             #print liststore[i][2]
         self.canvas.draw()
 
-    def label_toggle(self,widget):
+    def view_label_toggled(self,widget):
+        liststore = self.View.get_model()
+        if widget.get_active() == True:
+            for i in liststore:
+                xy = i[0]
+                label = i[1]
+                self.axes.text(self.data[1,xy],self.data[0,xy],label,fontsize=7)
+            
+        else:
+            pass 
+        self.canvas.draw()
+        
 
 
     def create_draw_frame(self,widget):
@@ -192,14 +205,14 @@ class setup_gui:
         #self.canvas.connect('button_press_event', self.button_press_event)
         self.canvas.show()
         self.figure = self.canvas.figure
-        self.axes = self.fig.add_axes([0.045, 0.05, 0.93, 0.925], axisbg='#FFFFCC')
-        self.axes.axis('off')
+        self.axes = self.fig.add_axes([0, 0, 1, 1], axisbg='#FFFFCC')
+        #self.axes.axis('off')
         self.vb = self.builder.get_object("viewport1")
         self.vb.add(self.canvas)
         #self.vb.pack_start(self.canvas, gtk.TRUE, gtk.TRUE)
         self.vb.show()
-        from pdf2py import readwrite
-        self.data = readwrite.readdata('/home/danc/vault/decrypted/programming/python/chanlocs.pym')
+        #from pdf2py import readwrite
+        #self.data = readwrite.readdata('/home/danc/vault/decrypted/programming/python/chanlocs.pym')
         #self.data = np.arange(300)#np.random.randn(300)
         self.axes.scatter(self.data[1],self.data[0],marker='o');#,facecolors='none');
 
@@ -210,9 +223,23 @@ class setup_gui:
             print(widget, event)
             m.show_all()
             m.popup(None,None,None,3,0)
+        if event.button == 1:
+            ap = self.axes.get_position()
+            x,y = self.canvas.get_width_height()
+            posx = (event.x/x-.5)*(1/(ap.x1-ap.x0))*-1
+            posy = ((event.y/y-.5)*(1/(ap.y0-ap.y1)))
+            #posx = ((event.x/x)-ap.x0)*(1/(ap.x1-ap.x0))
+            #posy = ((event.y/y)-(1-ap.y0))*(1/(ap.y0-ap.y1))
+            print posx,posy
+            from meg import nearest
+            nx=nearest.nearest(self.data[0],posy)[0]
+            ny=nearest.nearest(self.data[1],posx)[0]
+            print nx,ny
 
 if __name__ == "__main__":
-    mainwindow = setup_gui()
+    from pdf2py import readwrite
+    chanlocs = readwrite.readdata('/home/danc/python/chlocs.pym')
+    mainwindow = setup_gui(chanlocs)
     mainwindow.window.show()
     print 'testing'
     gtk.main()
